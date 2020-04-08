56c714db8bf3cb7bd868ca0f8a5919f8a344962a
Tac Tacelosky <tacman@gmail.com>
2020-04-08 09:00:00 -0400

    add and configure Survos Landing

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+const $ = require('jquery');
+require('bootstrap');
+const Popper = require('popper.js');
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
+/**
+\u0040fortawesome\/fontawesome\u002Dfree
+bootstrap
+fontawesome
+jquery
+popper.js
+**/
--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,8 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Survos\LandingBundle\SurvosLandingBundle::class => ['all' => true],
 ];
