88ca313b8ef66decb352fd69dac2f3ff46b58d33
Tac Tacelosky <tacman@gmail.com>
2020-09-04 10:02:46 -0400

    update libraries

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,11 @@
-// require('admin-lte/dist/js/adminlte');
+const $ = require('jquery');
+require('popper.js');
+global.jQuery = global.$ = $;
 
+console.log('app.js loading...');
+
+require('admin-lte'); // from yarn add admin-lte, 57k, does not include bootstrap
+require('bootstrap');
 require('../css/app.scss');
 
 // @todo: add to crud generator
