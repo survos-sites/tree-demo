9e67aba441ea99b57a341a729437bbf4d46113a8
Tac Tacelosky <tacman@gmail.com>
2021-11-24 14:04:17 -0500

    switch to new security

--- webpack.config.js
+++ webpack.config.js
@@ -20,15 +20,10 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
-    .addEntry('tree-api', './assets/js/tree-api.js')
-    .addEntry('building_show', './assets/js/building.js')
-    .addEntry('directory', './assets/js/directory.js')
-//    .addEntry('basic-ajax', './assets/js/basic-ajax.js')
-    .addEntry('basic-html', './assets/js/basic-html.js')
-//    .addEntry('LocationManager', './assets/js/LocationManagerApp.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addEntry('app', './assets/app.js')
+
+    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -61,7 +56,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    .enableSassLoader()
+    //.enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -77,11 +72,4 @@ Encore
     //.autoProvidejQuery()
 ;
 
-Encore.autoProvideVariables({
-    '$': 'jquery',
-    'jQuery': 'jquery',
-});
-
-const config = Encore.getWebpackConfig();
-config.resolve.symlinks = false;
-module.exports = config;
+module.exports = Encore.getWebpackConfig();
