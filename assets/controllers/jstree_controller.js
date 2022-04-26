2e493fa2ffc3e747ea576c21ee9a4a94354ce6a8
Tac Tacelosky <tacman@gmail.com>
2022-04-26 11:12:38 -0400

    add stimulus

---
+++ assets/controllers/jstree_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from 'stimulus';
+// import { jstree } from 'jstree';
+import 'jstree';
+
+export default class extends Controller {
+
+    static values = {
+        url: { type: String, default: '/bill' },
+        interval: { type: Number, default: 5 },
+        clicked: Boolean
+    }
+
+    static targets = [ "html", "ajax" ]
+
+    connect() {
+        this.element.textContent = 'Hello from ' + this.identifier;
+        if (this.hasHtmlTarget) {
+            this.html(htmlTarget);
+        }
+    }
+
+    html(el) {
+        $(el).jstree(
+            {
+                "plugins": ['checkbox', 'theme', "html_data", "types"]
+            }
+        );
+    }
+}
