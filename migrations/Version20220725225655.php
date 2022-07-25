d0f169832980a7385048c15258810e29033b3031
Tac Tacelosky <tacman@gmail.com>
2022-07-25 18:57:46 -0400

    update migration to include topic

--- migrations/Version20220725225655.php
+++ migrations/Version20220725225655.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220502122011 extends AbstractMigration
+final class Version20220725225655 extends AbstractMigration
 {
     public function getDescription(): string
     {
