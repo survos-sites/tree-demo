097a0f18576c8c3f3b2960cf49349589c19930f1
Tac Tacelosky <tacman@gmail.com>
2023-11-21 06:41:20 -0500

    update to 6.4, install assetmapper

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+// import './bootstrap.js';
+// /*
+//  * Welcome to your app's main JavaScript file!
+//  *
+//  * This file will be included onto the page via the importmap() Twig function,
+//  * which should already be in your base.html.twig.
+//  */
+// import './styles/app.css'
+// // import 'jstree/dist/themes/default/style.css';
+// import 'bootstrap'
+// console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+//
+// import 'bootstrap/dist/css/bootstrap.min.css'
